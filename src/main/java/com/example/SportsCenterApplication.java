bc1d5ef71f6815bfe933dd7612e34919c478c438
paliankit <paliankit999@gmail.com>
2025-07-28 15:16:41 +0530

    first commit

---
+++ src/main/java/com/example/SportsCenterApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsCenterApplication {
+    public static void main(String[] arguments){
+        SpringApplication.run(SportsCenterApplication.class,arguments);
+    }
+}
